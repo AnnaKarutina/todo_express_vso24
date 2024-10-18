4606367e258a6432000c3cdef2a658ed0f1ca510
Anna Karutina <anna.karutina@khk.ee>
2024-10-18 09:47:10 +0300

    Refactor data to JSON and update data reading and writing

--- index.js
+++ index.js
@@ -4,18 +4,61 @@ const fs = require('node:fs');
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            const tasks = JSON.parse(data) 
+            resolve(tasks)
+        });
+    })
+} 
+
 app.get('/', (req, res) => {
-    fs.readFile('./tasks', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        const tasks = data.split('\n') 
-        res.render('index', {tasks: tasks})
-    });
+    readFile('./tasks.json')
+        .then((tasks) => { 
+            res.render('index', {tasks: tasks})
+    })   
+})
+
+app.post('/', (req, res) => {
+    console.log('form sent data')
+    let task = req.body.task
+    readFile('./tasks.json')
+        .then((tasks) => {
+            let index
+            if(tasks.length === 0){
+                index = 0
+            } else {
+                index = tasks[tasks.length - 1].id + 1 
+            } 
+
+            const newTask = {
+                id: index,
+                task: task
+            } 
+
+            tasks.push(newTask)
+            console.log(tasks)
+            
+            const data = JSON.stringify(tasks, null, 2)
+
+            fs.writeFile('./tasks.json', data, err => {
+                if (err) {
+                  console.error(err);
+                } else {
+                    res.redirect('/')
+                }
+            });
+    })
 })
 
 app.listen(3001, () => {
